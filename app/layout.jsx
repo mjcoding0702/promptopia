d1143c9da209ed848f5c0ff5016c582871d66e5d
mjcoding0702 <chungmangjie2000@gmail.com>
2023-08-28 22:25:42 +0800

    Added HomePage

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,5 @@
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
 import '@styles/globals.css';
 
 export const metadata = {
@@ -9,13 +11,16 @@ const RootLayout = ({children}) => {
   return (
     <html lang="en">
         <body>
-            <div className='main'>
-                <div className='gradient'/>
-            </div>
+            <Provider>
+                <div className='main'>
+                    <div className='gradient'/>
+                </div>
 
-            <main className='app'>
-                {children}
-            </main>
+                <main className='app'>
+                    <Nav/>
+                    {children}
+                </main>
+            </Provider>
         </body>
     </html>
   )
